7b74abc56812284454f3e2e6306819501930ebd4
menelucas <115168314+menelucas@users.noreply.github.com>
2023-05-12 17:08:12 -0300

    Correções Gerais
    Corrigido o dotenv que não funcionava
    Corrigido o webpack
    Corrigido o Router que utilizava top level await
    Corrigida a responsividade da Home e do layout em geral

---
+++ dist/src_core_components_no-permission_no-permission_js.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkclient"] = self["webpackChunkclient"] || []).push([["src_core_components_no-permission_no-permission_js"],{
+
+/***/ "./src/@core/components/no-permission/no-permission.js":
+/*!*************************************************************!*\
+  !*** ./src/@core/components/no-permission/no-permission.js ***!
+  \*************************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* binding */ NoPermission; }\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ \"./node_modules/react-router-dom/dist/index.js\");\n/* harmony import */ var reactstrap__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! reactstrap */ \"./node_modules/reactstrap/esm/index.js\");\n\n\n\nfunction NoPermission() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"d-flex flex-column align-items-center justify-content-center text-dark\",\n    style: {\n      height: '80vh'\n    }\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"div\", {\n    className: \"h1\"\n  }, \"Voc\\xEA n\\xE3o tem permiss\\xE3o para acessar esta p\\xE1gina\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(reactstrap__WEBPACK_IMPORTED_MODULE_1__.Button, {\n    className: \"m-3\",\n    color: \"success\",\n    tag: react_router_dom__WEBPACK_IMPORTED_MODULE_2__.Link,\n    to: \"/login\"\n  }, \"Fazer login\"));\n}\n\n//# sourceURL=webpack://client/./src/@core/components/no-permission/no-permission.js?");
+
+/***/ })
+
+}]);

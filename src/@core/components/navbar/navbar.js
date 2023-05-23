b29a5ea8525ab229bd3b4cbb72933fb38e721803
zeezme <lukascass@gmail.com>
2023-05-22 22:58:20 -0300

    Correção no redirect do username

--- src/@core/components/navbar/navbar.js
+++ src/@core/components/navbar/navbar.js
@@ -24,6 +24,7 @@ import { Info, Menu } from 'react-feather'
 export default function KitNavbar({ screenWidth }) {
   const [cookies] = useCookies(['token'])
   const user = cookies['token']
+  const logged = useSelector((state) => state.login.user)
   const sidebarResponsive = useSelector((state) => state.user.sidebar_responsive)
   const dispatch = useDispatch()
   const location = useLocation()
@@ -33,8 +34,7 @@ export default function KitNavbar({ screenWidth }) {
   return (
     <Navbar
       style={{ backgroundColor: '#ededed', zIndex: '2', position: 'fixed', width: '100vw' }}
-      className=""
-    >
+      className="">
       <div className="w-100 d-flex flex-row justify-content-between">
         <div className="d-flex justify-content-start align-items-center">
           <NavbarBrand href="/">
@@ -46,8 +46,7 @@ export default function KitNavbar({ screenWidth }) {
             <Button
               color="transparent"
               className="d-flex flex-row justify-content-center align-items-center cursor-pointer link-none text-primary on-hover p-0 m-0 pt-1"
-              onClick={() => dispatch(togleSidebar(true))}
-            >
+              onClick={() => dispatch(togleSidebar(true))}>
               <Menu />
             </Button>
           )}
@@ -69,7 +68,7 @@ export default function KitNavbar({ screenWidth }) {
                   <p className="m-0 text-primary h5 fw-normal">Empresa</p>
                 </DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem tag={Link} to="/login">
+                <DropdownItem tag={Link} to={logged ? '/paywall' : '/login'}>
                   <p className="m-0 text-primary h5 fw-normal">
                     {user?.username ? capitalizeFirstLetter(user.username) : 'Login'}
                   </p>
@@ -81,15 +80,13 @@ export default function KitNavbar({ screenWidth }) {
             <div className="d-flex justify-content-center align-items-center text-dark ">
               <Button
                 color="transparent"
-                className="d-flex flex-column justify-content-center align-items-center cursor-pointer link-none text-primary on-hover"
-              >
+                className="d-flex flex-column justify-content-center align-items-center cursor-pointer link-none text-primary on-hover">
                 <p className="m-0 text-primary h5 fw-normal">Preços</p>
               </Button>
 
               <Button
                 color="transparent"
-                className="d-flex flex-column justify-content-center align-items-center cursor-pointer link-none text-primary on-hover"
-              >
+                className="d-flex flex-column justify-content-center align-items-center cursor-pointer link-none text-primary on-hover">
                 <p className="m-0 text-primary h5 fw-normal">Empresa</p>
               </Button>
             </div>
@@ -99,8 +96,7 @@ export default function KitNavbar({ screenWidth }) {
               color="transparent"
               tag={Link}
               className="d-flex flex-column justify-content-center align-items-center cursor-pointer link-none text-primary on-hover "
-              to="/login"
-            >
+              to={logged ? '/paywall' : '/login'}>
               <p className="m-0 text-primary h5 fw-normal">
                 {user?.username ? capitalizeFirstLetter(user.username) : 'Login'}
               </p>
